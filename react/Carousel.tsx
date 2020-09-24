8b21967b75823b8a4e6f5729ca29dea7c8b82891
klynger <rafael.klynger@gmail.com>
2020-09-24 15:29:28 -0300

    Create base carousel component

---
+++ react/Carousel.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import { ImageList } from 'vtex.store-image'
+import { SliderLayout } from 'vtex.slider-layout'
+
+const ITEMS_PER_PAGE = {
+  desktop: 1,
+  tablet: 1,
+  phone: 1,
+}
+
+function Carousel(props: Props) {
+  const { images } = props
+
+  const convertedImages = React.useMemo(
+    () =>
+      images.map(({ description, image, link }) => ({
+        description,
+        image,
+        mobileImage: image,
+        experimentalPreventLayoutShift: true,
+        link: link ?? {
+          url: link,
+          noFollow: false,
+        },
+      })),
+    [images]
+  )
+
+  return (
+    <ImageList images={convertedImages}>
+      <SliderLayout itemsPerPage={ITEMS_PER_PAGE} infinite />
+    </ImageList>
+  )
+}
+
+export default Carousel
+
+interface Props {
+  images: Image[]
+}
+
+interface Image {
+  image: string
+  description: string
+  link?: string
+}
